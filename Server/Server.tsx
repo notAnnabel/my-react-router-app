7a5698ac572f82ab4f3a57535c455b754fb46684
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-10-30 15:37:49 +0000

    aaaaaaaaaaa

---
+++ Server/Server.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { getData } from "../helpers/requests";
+
+
+
+export default function Server() {
+    const [serverText, setServerText] = useState("");
+
+    useEffect(() => {
+      async function fetchData(){
+        const data = await getData();
+        setServerText(data);}
+      
+
+      fetchData();}
+)
+  
+    return(
+        <div> The server says: {serverText}</div>
+    )
+};
---
+++ helpers/requests.ts
@@ -0,0 +1,17 @@
+export async function getData(){
+
+
+  const url = "https://localhost:3000";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.message
+  } catch (error: any) {
+    if (error && error.message)
+      console.error(error.message);
+  }
+}
