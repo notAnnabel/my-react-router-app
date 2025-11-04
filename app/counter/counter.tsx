e541df6058fe7672279a8848c54aee6f3f04fb9b
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-11-04 11:12:11 +0000

    yaaaaaaaaaaaaaaaaaaaaaaaauy

--- app/counter/counter.tsx
+++ app/counter/counter.tsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./counter.css"
+import { getCounter, postCounter } from "helpers/requests";
 
 
 export default function Counter() {
     const [counter, setCounter] = useState<number>(0);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getCounter();
+            setCounter(data);
+        }
+
+        fetchData();
+    }, []) // there are no dependencies
+
     function onClick() {
+        postCounter(counter + 1);
         setCounter(counter + 1);
     }
-    return(
+    return (
         <div className="flexbox">
             <h1>Counter is: {counter}</h1>
-            <button className='button' onClick ={onClick}>
+            <button className='button' onClick={onClick}>
                 add one
             </button>
         </div>
--- helpers/requests.ts
+++ helpers/requests.ts
@@ -15,3 +15,50 @@ export async function getData(){
       console.error(error.message);
   }
 }
+
+
+export async function getCounter(){
+
+
+  const url = "http://localhost:3000/counter";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.counter
+  } catch (error: any) {
+    if (error && error.message)
+      console.error(error.message);
+  }
+}
+
+
+
+
+
+export async function postCounter(counter: number){
+
+
+  const url = "http://localhost:3000/counter";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-type": "applications/json"
+      },
+      method: "POST",
+      body: JSON.stringify({counter: counter})
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.counter
+  } catch (error: any) {
+    if (error && error.message)
+      console.error(error.message);
+  }
+}
